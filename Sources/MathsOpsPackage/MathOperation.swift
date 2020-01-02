68beaab894928f8fb9987aadcb33658b2ebe49e4
cssandy <sandeep.shetty@avisbudget.com>
2020-01-02 16:57:20 +0530

    Multiply

--- Sources/MathsOpsPackage/MathOperation.swift
+++ Sources/MathsOpsPackage/MathOperation.swift
@@ -12,6 +12,9 @@ public class MathOperation {
     public static func addIntegers(num1:Int,num2:Int) -> Int {
         return num1 + num2
     }
+    public static func multiplyIntegers(num1:Int,num2:Int) -> Int {
+        return num1 * num2
+    }
     
    public static func getButton() -> UIImageView {
         let image = UIImageView(frame: CGRect(x: 50, y: 50, width: 300, height: 400))
